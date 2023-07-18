246599a852f5281a64a70d65bad9c1c4ede4bc2e
pluciuspua <e0957837@u.nus.edu>
2023-07-18 21:02:13 +0800

    updated files to match latest versions

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -4,26 +4,27 @@ const textInputDecoration = InputDecoration(
     fillColor: Color(0x191f252f),
     filled: true,
     enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.white, width: 2.0)
-    ),
+        borderSide: BorderSide(color: Colors.white, width: 2.0)),
     focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Color(0xff6deb4d), width: 2.0)
-    )
-);
+        borderSide: BorderSide(color: Color(0xff6deb4d), width: 2.0)));
 
 InputDecoration textInputDecoration2(String input) {
-    return InputDecoration(
-        hintText: input,
-        fillColor: backgroundColor,
-        filled: true,
-        enabledBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.white, width: 2.0)
-        ),
-        focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: appBarColor, width: 2.0)
-        )
-    );
+  return InputDecoration(
+      hintText: input,
+      fillColor: backgroundColor,
+      filled: true,
+      enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 2.0)),
+      focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: appBarColor, width: 2.0)));
 }
+
 const backgroundColor = Color.fromRGBO(224, 224, 224, 1);
 const appBarColor = Color.fromRGBO(98, 84, 138, 1);
 Color greyColor = Colors.grey.shade200;
+Widget gradientColor = Container(
+    decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+  Color.fromRGBO(123, 67, 151, 100),
+  Color.fromRGBO(220, 36, 48, 100)
+], begin: Alignment.topLeft, end: Alignment.bottomRight)));
