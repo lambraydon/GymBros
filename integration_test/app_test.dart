94c6fd2603067a01a0791adfc9a94bcd73d6c564
pluciuspua <e0957837@u.nus.edu>
2023-07-23 12:49:42 +0800

    Amended codemagic.yaml file for running automated test cases in Codemagic Amended test cases to init Firebase in the main() function

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:gymbros/main.dart';
 import 'package:integration_test/integration_test.dart';
 
-void main() {
+void main() async{
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   // Tests will go here...
   testWidgets(
